6c49fcef807a0ad04e728bacde8ef5f232dbe31f
EmmanuelHillary <emmanuelhillaryugwu@gmail.com>
2021-11-03 16:36:59 +0100

    03/11/2021

--- order/views.py
+++ order/views.py
@@ -3,9 +3,13 @@ from rest_framework import generics, mixins
 from rest_framework.response import Response
 from .serializers import OrderSerializer, OrderInlineSerializer
 from .models import Order
+from rest_framework.permissions import AllowAny
 
 
 class OrderListCreateAPIView(APIView):
+
+    permission_classes = [AllowAny]
+
     def get(self, *args, **kwargs):
         qs = Order.objects.all()
         serializer = OrderInlineSerializer(qs)
