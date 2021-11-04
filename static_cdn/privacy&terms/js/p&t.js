81c3ce0b0223064129abcf180de37a216199df6b
EmmanuelHillary <emmanuelhillaryugwu@gmail.com>
2021-11-04 03:44:21 +0100

    04/11/2021

---
+++ static_cdn/privacy&terms/js/p&t.js
@@ -0,0 +1,7 @@
+function openSlideMenu() {
+    document.getElementById('side').style.width = '250px';   
+}
+
+function closeSlideMenu() {
+    document.getElementById('side').style.width = '0';    
+}
